0bfc79025119535db453d37139de5f9358a7c51c
trifanuser <flc.tri@gmail.com>
2024-07-14 11:16:41 +0300

    update hw-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,25 @@
 
-// Funcție pentru a genera o culoare aleatorie în format hex
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
 
-// Selectăm butoanele
+
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 
 let intervalId;
 
-// Funcția pentru a începe schimbarea culorii de fundal
+
 function startChangingColor() {
-  startButton.disabled = true; // Dezactivăm butonul "Start"
+  startButton.disabled = true; 
   intervalId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 }
 
-// Funcția pentru a opri schimbarea culorii de fundal
 function stopChangingColor() {
-  clearInterval(intervalId); // Oprim schimbarea culorii
-  startButton.disabled = false; // Activăm butonul "Start"
+  clearInterval(intervalId);
+  startButton.disabled = false; 
 }
 
 // Adăugăm evenimentele click pentru butoane
