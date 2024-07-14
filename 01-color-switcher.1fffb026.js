45fa3c876d3cc81b9058ee8bf705c667fab088f5
trifanuser <158275008+trifanuser@users.noreply.github.com>
2024-07-14 07:39:29 +0000

    Deploying to gh-pages from @ trifanuser/goit-js-hw-09@0a07d1594c736ed9b2101531005b8725e524aec8 🚀

---
+++ 01-color-switcher.1fffb026.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a;t.addEventListener("click",(function(){t.disabled=!0,a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.1fffb026.js.map
---
+++ 01-color-switcher.49b199b0.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");e.addEventListener("click",(function(){e.disabled=!0,t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3)})),n.addEventListener("click",(function(){clearInterval(t),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.49b199b0.js.map
